eea6f97558f2d587d79f8401838c994c91eb9535
Obatola Opeyemi <opeyemiobatola@gmail.com>
2023-12-29 02:29:16 +0100

    added the functionality

---
+++ index.js
@@ -0,0 +1,19 @@
+const card = document.querySelector(".card");
+const adviceId = document.getElementById("advice-id");
+const adviceContainer = document.getElementById("advice");
+const btn = document.getElementById("btn");
+
+const url = "https://api.adviceslip.com/advice";
+
+btn.addEventListener("click", fetchAdvice);
+
+async function fetchAdvice() {
+  adviceContainer.classList.remove("fade");
+  const response = await fetch(url);
+  const data = await response.json();
+  adviceId.textContent = data.slip.id;
+  adviceContainer.textContent = data.slip.advice;
+  adviceContainer.classList.add("fade");
+}
+
+fetchAdvice();
